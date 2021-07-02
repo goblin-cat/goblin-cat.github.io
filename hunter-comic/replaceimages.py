51d95056be87e272529bde0f5d66e7b82502c025
xluo1 <86752883+goblin-cat@users.noreply.github.com>
2021-07-02 18:50:14 -0400

    replaced images

--- hunter-comic/replaceimages.py
+++ hunter-comic/replaceimages.py
@@ -24,4 +24,108 @@ for i in range(0,numPages):
     
     with open(path, "w") as ogfile:
         ogfile.write(str(soup))
-    break
+        
+##
+
+
+ch2Files=os.listdir('ch2')
+numPages = len(ch2Files)
+
+for i in range(0,numPages):
+    path='ch2//'+ch2Files[i]
+    file = open(path,'r')
+    page = file.read()
+    soup = BeautifulSoup(page, 'lxml')
+    
+    myImages = soup.findAll('img',{"class": "panel"})
+    
+    for i in range(0,len(myImages)):
+        imageCounter += 1
+        myImages[i]["src"]='/hunter-comic/panels/'+str(imageCounter) + '.png'
+    
+    with open(path, "w") as ogfile:
+        ogfile.write(str(soup))
+
+# done w ch2
+
+
+ch3Files=os.listdir('ch3')
+numPages = len(ch3Files)
+
+for i in range(0,numPages):
+    path='ch3//'+ch3Files[i]
+    file = open(path,'r')
+    page = file.read()
+    soup = BeautifulSoup(page, 'lxml')
+    
+    myImages = soup.findAll('img',{"class": "panel"})
+    
+    for i in range(0,len(myImages)):
+        imageCounter += 1
+        myImages[i]["src"]='/hunter-comic/panels/'+str(imageCounter) + '.png'
+    
+    with open(path, "w") as ogfile:
+        ogfile.write(str(soup))
+
+
+
+ch4Files=os.listdir('ch4')
+numPages = len(ch4Files)
+
+for i in range(0,numPages):
+    path='ch4//'+ch4Files[i]
+    file = open(path,'r')
+    page = file.read()
+    soup = BeautifulSoup(page, 'lxml')
+    
+    myImages = soup.findAll('img',{"class": "panel"})
+    
+    for i in range(0,len(myImages)):
+        imageCounter += 1
+        myImages[i]["src"]='/hunter-comic/panels/'+str(imageCounter) + '.png'
+    
+    with open(path, "w") as ogfile:
+        ogfile.write(str(soup))
+
+
+ch5Files=os.listdir('ch5')
+numPages = len(ch5Files)
+
+for i in range(0,numPages):
+    path='ch5//'+ch5Files[i]
+    file = open(path,'r')
+    page = file.read()
+    soup = BeautifulSoup(page, 'lxml')
+    
+    myImages = soup.findAll('img',{"class": "panel"})
+    
+    for i in range(0,len(myImages)):
+        imageCounter += 1
+        myImages[i]["src"]='/hunter-comic/panels/'+str(imageCounter) + '.png'
+    
+    with open(path, "w") as ogfile:
+        ogfile.write(str(soup))
+
+
+ch6Files=os.listdir('ch6')
+numPages = len(ch6Files)
+
+for i in range(0,numPages):
+    path='ch6//'+ch6Files[i]
+    file = open(path,'r')
+    page = file.read()
+    soup = BeautifulSoup(page, 'lxml')
+    
+    myImages = soup.findAll('img',{"class": "panel"})
+    
+    for i in range(0,len(myImages)):
+        imageCounter += 1
+        myImages[i]["src"]='/hunter-comic/panels/'+str(imageCounter) + '.png'
+    
+    with open(path, "w") as ogfile:
+        ogfile.write(str(soup))
+
+
+            
+
+
